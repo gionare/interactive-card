d1de4d52ad21000aaf751da5c51d7095abf05ced
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-03-13 12:20:52 -0400

    add useState Hook & handleSubmit function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import CardFillUp from "./components/CardFillUp";
+import FillupForm from "./components/FillupForm";
 
 function App() {
   return (
     <>
-      <CardFillUp></CardFillUp>
+      <FillupForm></FillupForm>
     </>
   );
 }
---
+++ src/components/FillupForm.jsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+export default function FillupForm() {
+  const [inputs, setInputs] = useState({});
+
+  // https://www.w3schools.com/react/react_forms.asp
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const cardNumber = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(inputs);
+  };
+
+  return (
+    <CardFillUpStyled onSubmit={handleSubmit}>
+      <label htmlFor="cardholder-name">
+        Cardholder Name
+        <input
+          type="text"
+          placeholder="e.g. Giorgi Nareklishvili"
+          value={inputs.cardHolderNames}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="card-number">
+        Card Number
+        <input
+          type="text"
+          placeholder="e.g. 1234 5678 9123 0000"
+          value={inputs.cardNumbers}
+          onChange={handleChange}
+        />
+      </label>
+      <div className="datendcvc-div">
+        <label htmlFor="exp-date">
+          Exp. Date (MM/YY)
+          <input type="number" placeholder="MM" onChange={handleChange} />
+          <input type="number" placeholder="YY" onChange={handleChange} />
+        </label>
+        <label htmlFor="cvc">
+          CVC
+          <input type="number" placeholder="e.g. 123" onChange={handleChange} />
+        </label>
+      </div>
+      <button onClick={handleSubmit}>Confirm</button>
+    </CardFillUpStyled>
+  );
+}
+
+const CardFillUpStyled = styled.form`
+  width: 375px;
+  padding: 0 0 45px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #fff;
+  gap: 10px;
+
+  label {
+    width: 140px;
+    font-size: 12px;
+    font-weight: 500;
+    letter-spacing: 2px;
+    padding: 0px 45px 10px 0px;
+    text-align: left;
+    color: #21092f;
+  }
+
+  input {
+    width: 327px;
+    height: 45px;
+    border-radius: 8px;
+    border: solid 1px #dfdee0;
+    background-color: #fff;
+  }
+  .datendcvc-div {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+
+    input {
+      width: 72px;
+    }
+  }
+
+  button {
+    width: 327px;
+    height: 53px;
+    margin: 28px 0 0;
+    padding: 15px 130px 15px 129px;
+    border-radius: 8px;
+    background-color: #21092f;
+  }
+`;
