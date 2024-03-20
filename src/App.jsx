ebae615a38d67c976d45afe1709966708e034a61
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-03-19 23:14:18 -0400

    adding header for Live Card function

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import "./App.css";
 import FillupForm from "./components/FillupForm";
+import HeaderLiveCard from "./components/HeaderLiveCard";
 
 function App() {
   return (
     <>
+      <HeaderLiveCard></HeaderLiveCard>
       <FillupForm></FillupForm>
     </>
   );
---
+++ src/components/HeaderLiveCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function HeaderLiveCard() {
+  return (
+    <>
+      <img src="public/images/bg-main-mobile.png" alt="" />
+    </>
+  );
+}
