0622c2ea21b0420a134ff8856b30af88d773cbec
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-03-27 00:46:24 -0400

    absolute and relative

--- src/components/HeaderLiveCard.jsx
+++ src/components/HeaderLiveCard.jsx
@@ -3,8 +3,35 @@ import styled from "styled-components";
 
 export default function HeaderLiveCard() {
   return (
-    <>
-      <img src="public/images/bg-main-mobile.png" alt="" />
-    </>
+    <Livecards>
+      <div>
+        <img
+          className="relative-image"
+          src="public/images/bg-main-mobile.png"
+          alt=""
+        />
+      </div>
+      <div>
+        <img
+          classname="absolute-image"
+          src="public/images/bg-card-back.png"
+          alt=""
+        />
+      </div>
+    </Livecards>
   );
 }
+
+const Livecards = styled.div`
+  .relative-image {
+    position: relative;
+  }
+  .absolute-image {
+    position: absolute;
+    top: 80px;
+    right: 0;
+    width: 100px;
+    height: 100px;
+    border: 3px solid #73ad21;
+  }
+`;
